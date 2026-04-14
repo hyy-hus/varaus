aeeb0fd0bc7693aed64c76e750319bd6d96e70ca
Janne Saranpää <janne@jssa.fi>
2026-04-14 19:23:14 +0300

    use router in main

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from core.database import get_session
 
+from api import resources
+
 app = FastAPI(
     title="Varaus API", description="API for reserving things", version="0.0.0"
 )
@@ -31,3 +33,6 @@ async def root():
 def test_db_connection(session: Session = Depends(get_session)):
     result = session.exec(select(1)).first()
     return {"db_connected": result == 1}
+
+
+app.include_router(resources.router, prefix="/api/resources", tags=["Resources"])
