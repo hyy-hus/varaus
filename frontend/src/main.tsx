0efec98846b0de048c86c96ccdc8acafe645b712
Janne Saranpää <janne@jssa.fi>
2026-04-15 09:42:45 +0300

    feat: add shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,39 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { ThemeProvider } from "@/components/themeProvider"
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5000,
+        },
+    },
+})
 
 const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  scrollRestoration: true,
+    routeTree,
+    defaultPreload: 'intent',
+    scrollRestoration: true,
 })
 
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+    interface Register {
+        router: typeof router
+    }
 }
 
 const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </ThemeProvider>
+    )
 }
