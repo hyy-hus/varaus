e032daed003a6101f5e394a2296851a8bf46a9f4
Janne Saranpää <janne@jssa.fi>
2026-04-15 09:41:19 +0300

    chore: update deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,24 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 const config = defineConfig({
-    resolve: { tsconfigPaths: true },
+    resolve: { tsconfigPaths: true, alias: { "@": path.resolve(__dirname, "./src"), } },
     plugins: [
         devtools(),
         tailwindcss(),
         tanstackRouter({ target: 'react', autoCodeSplitting: true }),
         viteReact(),
     ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            }
+        }
+    }
 })
 
 export default config
